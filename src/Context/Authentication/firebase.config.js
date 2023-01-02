a08f0bdc7980e9bed495a26edb7c0902036ef771
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-02 12:22:11 +0600

    setup folder structer

---
+++ src/Context/Authentication/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSeJHV206FYRon-viWWB_kqP5CGZS2QrY",
+  authDomain: "ark-food.firebaseapp.com",
+  projectId: "ark-food",
+  storageBucket: "ark-food.appspot.com",
+  messagingSenderId: "1016785170670",
+  appId: "1:1016785170670:web:9fab48f457e270680c83ff"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
